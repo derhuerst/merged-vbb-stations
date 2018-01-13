c6cdb45148e114c1cc3e713c0058110f606326a5
Jannis R <mail@jannisr.de>
2018-01-13 22:27:04 +0100

    tests :white_check_mark:

--- test.js
+++ test.js
@@ -2,7 +2,16 @@
 
 const test = require('tape')
 
-const todo = require('.')
+const merged = require('.')
 
-test('todo', (t) => {
+test('contains "Singerstr." as unmerged', (t) => {
+	t.plan(1)
+	// Singerstr.
+	t.equal(merged['900000120022'], '900000120022')
+})
+
+test('contains "U Kottbusser Tor [Bus Adalbertstr.]" as merged', (t) => {
+	t.plan(1)
+	// U Kottbusser Tor [Bus Adalbertstr.] -> U Kottbusser Tor
+	t.equal(merged['900000013172'], '900000013102')
 })
