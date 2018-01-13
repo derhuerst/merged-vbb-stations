dff9da7e1a6d412fe4c09d47b2baefa7792de844
Jannis R <mail@jannisr.de>
2018-01-13 23:01:44 +0100

    store & publish merge instructions as well

--- build.js
+++ build.js
@@ -1,7 +1,14 @@
 'use strict'
 
 const applyToAll = require('merge-vbb-stations/lib/apply-to-all')
+const fs = require('fs')
+const path = require('path')
 
-const {mapping} = applyToAll()
+const {mapping, instructions} = applyToAll()
 
-process.stdout.write(JSON.stringify(mapping.toObject()) + '\n')
+const writeJSON = (file, data) => {
+	fs.writeFileSync(path.join(__dirname, file), JSON.stringify(data) + '\n')
+}
+
+writeJSON('index.json', mapping.toObject())
+writeJSON('instructions.json', instructions)
