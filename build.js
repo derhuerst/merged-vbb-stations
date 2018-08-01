98c87afa64451f9b1cde89f886ddb368ffb9ccd8
Jannis R <mail@jannisr.de>
2018-08-01 18:00:24 +0200

    print instructions on build

--- build.js
+++ build.js
@@ -1,10 +1,18 @@
 'use strict'
 
 const applyToAll = require('merge-vbb-stations/lib/apply-to-all')
+const analyze = require('merge-vbb-stations')
 const fs = require('fs')
 const path = require('path')
 
 const {mapping, instructions} = applyToAll()
+
+for (let instruction of instructions) {
+	const {op, src, dest, stopName} = instruction
+	if (op === analyze.MERGE) {
+		console.info(src.id, src.name, 'as', stopName, 'into', dest.id, dest.name)
+	} else console.error('unknown merge operation:', op)
+}
 
 const writeJSON = (file, data) => {
 	fs.writeFileSync(path.join(__dirname, file), JSON.stringify(data) + '\n')
